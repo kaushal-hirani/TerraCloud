64bd6383f01385d112dff3d9561f3aa837c186a1
Kaushal Hirani <kaushalhirani007@gmail.com>
2023-08-10 23:32:33 +0530

    Added Route table and association

--- main.tf
+++ main.tf
@@ -16,3 +16,25 @@ resource "aws_subnet" "sub2" {
   map_public_ip_on_launch = true
 
 }
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.new_vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.new_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.sub1.id
+  route_table_id = aws_route_table.rt.id
+}
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.sub2.id
+  route_table_id = aws_route_table.rt.id
+}
